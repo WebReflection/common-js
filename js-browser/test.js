26e6ce588d8e9e2a60230b22c27b5c37e8155c7d
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-01-20 22:05:14 +0000

    added module exporting Promise to defer import

--- js-browser/test.js
+++ js-browser/test.js
@@ -1,3 +1,17 @@
+// exports can be either synchronous
 module.exports = function (message) {
-  alert(message + '\nfrom ' + module.filename);
+  alert(message);
 };
+
+// or even asynchronous
+/*
+module.exports = new Promise(function (resolve) {
+  setTimeout(
+    resolve,
+    1000,
+    function (message) {
+      alert(message + '\nfrom ' + module.filename);
+    }
+  );
+});
+*/
